545df7c069f8bd64bb9e6a763270bb66d02c69b1
tang_huibo <1215893264@qq.com>
2021-08-01 00:17:14 +0800

    basic init

---
+++ fastjson-test/src/main/java/io/github/tanghuibo/fastjsontest/FastjsonTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.fastjsontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastjsonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastjsonTestApplication.class, args);
+	}
+
+}
---
+++ fastjson-test/src/test/java/io/github/tanghuibo/fastjsontest/FastjsonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tanghuibo.fastjsontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastjsonTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
